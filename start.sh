27300791b3ff37b687bd5dca644dfe597bb05ab3
Anakri <69618193+Anakr1@users.noreply.github.com>
2024-11-17 09:01:52 +0300

    Добавил стартовый файл

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+sudo apt install zsh lsd neovim mc
+mv dotfiles .dotfiles
+rm ~/.zshrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.config ~/.config
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Open nvim and text :PlugInstall'"
+echo "Finish

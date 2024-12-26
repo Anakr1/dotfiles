60736b639258d7127ac62c978330b539d4b19e9e
Anakr1 <zazoo12613@gmail.com>
2024-12-26 20:57:44 +0300

    edit start3

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@
 
 sudo apt install zsh lsd neovim mc
 cp -r ~/.dotfiles oldfiles/
-rm ~/.dotfiles
+rm -rf ~/.dotfiles
 cp .dotfiles ~/.dotfiles
 mkdir oldfiles
 cp -r ~/.zshrc oldfiles/
-rm ~/.zshrc
+rm -rf ~/.zshrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 cp -r ~/.config oldfiles/
-rm ~/.config
+rm -rf ~/.config
 ln -s ~/.dotfiles/.config ~/.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'

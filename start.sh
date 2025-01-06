1e0911331adaf607092c690e39426615541a759c
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 10:40:06 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,5 +14,7 @@ rm -rf ~/.config
 ln -s ~/.dotfiles/.config ~/.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Open nvim and text :PlugInstall'"   # установка плагинов
 echo "Finish"

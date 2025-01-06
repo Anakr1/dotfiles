58e3557d4e83692c2134cade12ac69440dfff492
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 11:25:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 
 sudo apt install zsh neovim mc bat ripgrep
 mkdir oldfiles
-cp -r ../.dotfiles oldfiles/
-sudo rm -rf ../.dotfiles
-cp -r .dotfiles ../.dotfiles
-cp -r ../.zshrc oldfiles/
-sudo rm -rf ../.zshrc
-ln -s ../.dotfiles/.zshrc ../.zshrc
-cp -r ../.config oldfiles/
-sudo rm -rf ../.config
-ln -s ../.dotfiles/.config ../.config
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cp -r ~/.dotfiles oldfiles/
+sudo rm -rf ~/.dotfiles
+cp -r .dotfiles ~/.dotfiles
+cp -r ~/.config oldfiles/
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp -r ~/.zshrc oldfiles/
+sudo rm -rf ~/.zshrc
+ln -s .dotfiles/.zshrc ../.zshrc
+sudo rm -rf ~/.config
+ln -s .dotfiles/.config ../.config
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Open nvim and text :PlugInstall'"   # установка плагинов
 echo "Finish"
 

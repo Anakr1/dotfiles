3f351e5f521522ef4be87c769368b4165c90b7d8
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 11:44:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,20 +2,16 @@
 
 sudo apt install zsh neovim mc bat ripgrep
 mkdir oldfiles
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -r ~/.dotfiles oldfiles/
 sudo rm -rf ~/.dotfiles
 cp -r .dotfiles ~/.dotfiles
 cp -r ~/.config oldfiles/
-cp -r ~/.zshrc oldfiles/
-rm -rf ~/.zshrc
-ln -s .dotfiles/.zshrc ../.zshrc
 rm -rf ~/.config
 ln -s .dotfiles/.config ../.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Open nvim and text :PlugInstall'"   # установка плагинов
-echo "Finish"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #sudo apt install lsd
+#snap install lsd

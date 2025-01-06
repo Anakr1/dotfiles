5238e36db851bb05507f6c1ea7372ba5d035aed2
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 11:40:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 
 sudo apt install zsh neovim mc bat ripgrep
 mkdir oldfiles
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -r ~/.dotfiles oldfiles/
 sudo rm -rf ~/.dotfiles
 cp -r .dotfiles ~/.dotfiles
 cp -r ~/.config oldfiles/
-chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -r ~/.zshrc oldfiles/
 rm -rf ~/.zshrc
 ln -s .dotfiles/.zshrc ../.zshrc

2396aab5f62159611c08156f544218bccffa2564
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 11:46:13 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,8 @@
 cp -r ~/.zshrc oldfiles/
 rm -rf ~/.zshrc
 ln -s .dotfiles/.zshrc ../.zshrc
-echo 
+
+echo "Open nvim and text :PlugInstall'"   # установка плагинов
+echo "Finish"
+#sudo apt install lsd
+#snap install lsd

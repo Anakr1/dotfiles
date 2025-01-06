a2dd05022b0e4452cedc8aef7f0d5fc2ff8941d9
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 11:45:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,8 +10,6 @@ rm -rf ~/.config
 ln -s .dotfiles/.config ../.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-echo "Open nvim and text :PlugInstall'"   # установка плагинов
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#sudo apt install lsd
-#snap install lsd
+#use second script

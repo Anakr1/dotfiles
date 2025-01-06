e3f24ddb3072c68cc70193b125f02f2b1a32690e
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-01-06 10:59:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
 sudo apt install zsh neovim mc bat ripgrep
 mkdir oldfiles
-cp -r ~/.dotfiles oldfiles/
-sudo rm -rf ~/.dotfiles
-cp -r .dotfiles ~/.dotfiles
-cp -r ~/.zshrc oldfiles/
-sudo rm -rf ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-cp -r ~/.config oldfiles/
-sudo rm -rf ~/.config
-ln -s ~/.dotfiles/.config ~/.config
+cp -r ../.dotfiles oldfiles/
+sudo rm -rf ../.dotfiles
+cp -r .dotfiles ../.dotfiles
+cp -r ../.zshrc oldfiles/
+sudo rm -rf ../.zshrc
+ln -s ../.dotfiles/.zshrc ../.zshrc
+cp -r ../.config oldfiles/
+sudo rm -rf ../.config
+ln -s ../.dotfiles/.config ../.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 chsh -s $(which zsh)

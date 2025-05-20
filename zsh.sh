2989134c862ad070bdb2d80b77e37288827484d2
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-05-20 15:40:55 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,8 @@
 mv ~/.zshrc oldfiles/
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
 echo "Open nvim and text :PlugInstall'"   # установка плагинов
 echo "Finish"

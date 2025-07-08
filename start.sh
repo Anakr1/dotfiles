ea2c8356a0bd79099861748adcfd37acaee8ffee
Anakri <69618193+Anakr1@users.noreply.github.com>
2025-07-08 21:28:50 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,9 @@ mv ~/.dotfiles oldfiles/
 cp -r .dotfiles ~/.dotfiles
 mv ~/.config oldfiles/
 ln -s ~/.dotfiles/.config ~/.config
-#curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-#mkdir ~/.soft
-#tar -C ~/.soft -xzf nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+mkdir ~/.soft
+tar -C ~/.soft -xzf nvim-linux64.tar.gz
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

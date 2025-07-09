1d04c8bfaeb62a56b255898c060550d397832184
test <test>
2025-07-09 20:26:22 +0300

    master: update

--- install.sh
+++ install.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
-#sudo apt install curl wget bash-completion zsh ncdu lsd tree ripgrep 
-set -v
+#
+# Скрипт установки базового окружения:
+# - Настраивает необходимые директории
+# - Копирует утилиты и конфиги
+# - Создает симлинки для конфигурационных файлов
+# - Устанавливает ZSH как оболочку по умолчанию
 
-# подготовка папок 
-mkdir -p ~/.local/bin/
-mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
+#sudo apt install curl wget bash-completion zsh ncdu lsd tree ripgrep
 
-# копирование файлов
-cp -p ./files/plug.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
-cp ./files/zoxide ~/.local/bin/zoxide
-cp ./files/rg ~/.local/bin/rg
-cp ./files/lsd ~/.local/bin/lsd
 
-# создание линков
-rm -rf ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-mkdir -p ~/.config
-ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
-ln -s ~/.dotfiles/.config/zsh_functions ~/.config/zsh_functions
-ln -s ~/.dotfiles/files/oh-my-zsh-offline ~/.oh-my-zsh
+# ===== 1. Подготовка директорий =====
+mkdir -p "${HOME}/.local/bin"
+mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload"
+
+# ===== 2. Копирование файлов =====
+# Копирование с сохранением прав (если файлы существуют)
+[ -f "./files/plug.vim" ] && \
+    cp -p "./files/plug.vim" "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
+[ -f "./files/zoxide" ] && cp "./files/zoxide" "${HOME}/.local/bin/zoxide"
+[ -f "./files/rg" ] && cp "./files/rg" "${HOME}/.local/bin/rg"
+[ -f "./files/lsd" ] && cp "./files/lsd" "${HOME}/.local/bin/lsd"
+
+# ===== 3. Создание симлинков =====
+# Удаляем старый .zshrc, если есть
+rm -rf "${HOME}/.zshrc"
+# Создаем симлинки на конфиги
+ln -s "${HOME}/.dotfiles/.zshrc" "${HOME}/.zshrc"
+mkdir -p "${HOME}/.config"
+ln -s "${HOME}/.dotfiles/.config/nvim" "${HOME}/.config/nvim"
+ln -s "${HOME}/.dotfiles/.config/zsh_functions" "${HOME}/.config/zsh_functions"
+ln -s "${HOME}/.dotfiles/files/oh-my-zsh-offline" "${HOME}/.oh-my-zsh"
+
+# ===== 4. Установка ZSH как оболочки по умолчанию =====
+if command -v zsh >/dev/null 2>&1; then
+    chsh -s "$(command -v zsh)"
+else
+    echo "Error: zsh is not installed!" >&2
+    exit 1
+fi
 
 #git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-#git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+#git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git 
 
-chsh -s `which zsh`
 
-echo "Open nvim and text :PlugInstall'"   # установка плагинов
-echo "Finish"
-echo ""
+# ===== 5. Завершение =====
+echo "Установка завершена."
+echo "1. Откройте Neovim и выполните :PlugInstall для установки плагинов"
+echo "2. Перезайдите в систему для применения ZSH"

27ddedb4b9c644f2cd2d5f574e15dda144103a18
test <test>
2025-07-09 20:02:15 +0300

    master: update

--- install.sh
+++ install.sh
@@ -24,6 +24,8 @@ ln -s ~/.dotfiles/files/oh-my-zsh-offline ~/.oh-my-zsh
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 #git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
+chsh -s `which zsh`
+
 echo "Open nvim and text :PlugInstall'"   # установка плагинов
 echo "Finish"
 echo ""
